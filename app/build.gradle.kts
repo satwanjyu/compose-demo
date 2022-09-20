6a2e208d7bb93eedc1bc92bc01f596ce42751790
失魂魚 <satwanjyu@outlook.com>
2022-09-20 09:55:32 +0800

    Ktor, ContentNegotiation, KTX-Serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "1.7.0"
 }
 
 val composeVersion = "1.2.0"
@@ -51,13 +52,22 @@ android {
     }
 }
 
+val ktorVersion = "2.1.1"
+
 dependencies {
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-android:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.5.1")
     implementation("androidx.compose.ui:ui:1.2.1")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.0.0-beta02")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
--- app/src/main/java/io/github/satwanjyu/demo/MainActivity.kt
+++ app/src/main/java/io/github/satwanjyu/demo/MainActivity.kt
@@ -3,16 +3,42 @@ package io.github.satwanjyu.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.satwanjyu.demo.ui.theme.DemoTheme
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +48,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LiveReleases(client = client)
                 }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        client.close()
+    }
+}
+
+@Composable
+fun LiveReleases(client: HttpClient) {
+    val releases = remember { mutableStateListOf<Release>() }
+
+    LaunchedEffect(releases) {
+        val response =
+            client
+                .get("https://api.github.com/repos/jetbrains/kotlin/tags")
+                .body<List<Release>>()
+        releases.clear()
+        releases.addAll(response)
+    }
+
+    Releases(releases)
+}
+
+@Serializable
+data class Release(val name: String)
+
+@Composable
+fun Releases(releases: List<Release>) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("Kotlin Releases:")
+
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            items(releases) {
+                Text(it.name, modifier = Modifier.padding(top = 8.dp))
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewReleases() {
+    val releases = List(100) { Release("Kotlin 1.0.$it") }
     DemoTheme {
-        Greeting("Android")
+        Releases(releases)
     }
 }
